39d9600f38beaf26d921e14bf20f48a4c5cb1437
Sahandevs <sahandevs@gmail.com>
2020-02-03 12:28:29 +0330

    add testAll helper function

---
+++ packages/xstate/test/machine_tester.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import 'package:xstate/xstate.dart';
+
+testAll<T>(Machine machine, Map<T, Map<dynamic, T>> expected) {
+  expected.forEach((state, target) {
+    target.forEach((event, targetState) {
+      final result = machine.transition(state, event);
+      expect(
+        result,
+        targetState,
+        reason:
+            "Transition is [$state---$event--->$result].But it should be [$state---$event--->]$targetState",
+      );
+    });
+  });
+}
