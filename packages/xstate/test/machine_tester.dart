18b1a07657f3a73ca262beca5d68bfcb85eefc60
Sahandevs <sahandevs@gmail.com>
2020-02-04 13:38:36 +0330

    add support for multi events in testAll

--- packages/xstate/test/machine_tester.dart
+++ packages/xstate/test/machine_tester.dart
@@ -3,14 +3,17 @@ import 'package:xstate/xstate.dart';
 
 testAll<T>(Machine machine, Map<T, Map<dynamic, T>> expected) {
   expected.forEach((state, target) {
-    target.forEach((event, targetState) {
-      final result = machine.transition(state, event);
-      expect(
-        result.matches(targetState),
-        true,
-        reason:
-            "Transition is [$state---$event--->${result.describe()}].But it should be [$state---$event--->$targetState]",
-      );
+    target.forEach((events, targetState) {
+      final String _events = events;
+      _events.split(",").map((x) => x.trim()).forEach((event) {
+        final result = machine.transition(state, event);
+        expect(
+          result.matches(targetState),
+          true,
+          reason:
+              "Transition is [$state---$event--->${result.describe()}].But it should be [$state---$event--->$targetState]",
+        );
+      });
     });
   });
 }
