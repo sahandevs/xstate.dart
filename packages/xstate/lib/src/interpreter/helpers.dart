51fc986c519c437196256865a6ba79fa0a44ca9b
Sahandevs <sahandevs@gmail.com>
2020-02-12 21:24:42 +0330

    implement getProperAncestors helper method

--- packages/xstate/lib/src/interpreter/helpers.dart
+++ packages/xstate/lib/src/interpreter/helpers.dart
@@ -1,6 +1,27 @@
 part of 'interpreter.dart';
 
+/// If [state2] is null, returns the set of all ancestors of [state1] in ancestry order
+/// ([state1]'s parent followed by the parent's parent, etc. up to an including the <scxml> element).
+/// If [state2] is non-null, returns in ancestry order the set of all ancestors of [state1], up to but
+/// not including [state2]. (A "proper ancestor" of a state is its parent, or the parent's parent, or
+/// the parent's parent's parent, etc.))If [state2] is [state1]'s parent, or equal to [state1], or a descendant
+/// of [state1], this returns the empty set.
+LinkedHashSet<SCXMLElement> getProperAncestors(
+    SCXMLElement state1, SCXMLElement state2) {
+  assert(state1 != null && state1 is IState);
+  assert(state2 == null || state2 is IState);
 
+  if (state1 == state2 || state1.parent == state2) return LinkedHashSet();
+
+  if (state1 is StateWithChildren) if (isDescendant(
+      state2, state1 as StateWithChildren)) return LinkedHashSet();
+  // TODO: what should happen when [state2] is not proper ancestor of state1 ?
+  final result = LinkedHashSet<SCXMLElement>();
+  if (state1.parent == null) return result;
+  result.add(state1.parent);
+  result.addAll(getProperAncestors(state1.parent, state2));
+  return result;
+}
 
 /// Returns 'true' if [state] is a descendant of [parent]
 /// (a child, or a child of a child, or a child of a child of a child, etc.)
